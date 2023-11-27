346092d72c909b863d20d52ccaf64d6cad8a7fc1
Khitrov <Hitrov.95@yandex.ru>
2023-11-27 15:17:19 +0300

    денис_хитров_4_3_1_дз

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class TestStack(unittest.TestCase):
+    def test_push_pop(self):
+        """Тестирование добавления и удаления элементов"""
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.pop(), 3)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.pop(), 1)
+
+    def test_node_creation(self):
+        """Тестирование создания экземпляров"""
+        node = Node(5, None)
+        self.assertEqual(node.data, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
